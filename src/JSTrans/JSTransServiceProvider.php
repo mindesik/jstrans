9f228fd12ef28ac9acbd243798f3431ad08018a5
Eugene Min <e.min@milax.com>
2016-06-05 01:34:58 +1100

    New functionality

--- src/JSTrans/JSTransServiceProvider.php
+++ src/JSTrans/JSTransServiceProvider.php
@@ -4,6 +4,8 @@ namespace MisterPaladin\JSTrans;
 
 use Illuminate\Support\ServiceProvider;
 use Blade;
+use Lang;
+use View;
 
 class JSTransServiceProvider extends ServiceProvider
 {
@@ -16,11 +18,27 @@ class JSTransServiceProvider extends ServiceProvider
     
     public function register()
     {
-        Blade::directive('jstrans', function ($expression) {
+        $this->publishes([
+            __DIR__ . '/../config.php' => config_path('jstrans.php'),
+        ], 'jstrans');
+        
+        Blade::directive('jstrans', function ($expression) use (&$trans) {
+            $trans = [];
+            
+            foreach (\Config::get('jstrans') as $lang) {
+                $trans[$lang] = Lang::get($lang);
+            }
+            
+            $trans = json_encode($trans);
+            
+            return "<?php echo '<textarea name=\"jstrans\" disabled readonly>{$trans}</textarea><script src=\"/js/jstrans.js\" type=\"text/javascript\"></script>'; ?>";
+        });
+        
+        Blade::directive('jstransval', function ($expression) {
             return '<?php
                 $exp = ' . $expression . ';
                 foreach ($exp as $key => $value) {
-                    echo "<input type=\"hidden\" disabled=\"disabled\" name=\"jstrans-$key\" value=\"$value\" />";
+                    echo "<input type=\"text\" disabled=\"disabled\" name=\"jstrans-$key\" value=\"$value\" />";
                 }
             ?>';
         });
--- src/js/jstrans.js
+++ src/js/jstrans.js
@@ -1,7 +1,26 @@
-var jstrans = function (key) {
-    if (selector = document.querySelector('input[name="jstrans-' + key + '"]')) {
+var jstrans = function (path) {
+    // First try input
+    if (selector = document.querySelector('input[name="jstrans-' + path + '"]')) {
         return selector.value;
     } else {
-        return null;
+        // Or textarea with huge languages array
+        if (selector = document.querySelector('textarea[name="jstrans"]')) {
+            var json = JSON.parse(selector.value);
+            var keys = path.split('.');
+            
+            var value = json;
+            
+            for (var i in keys) {
+                var key = keys[i];
+                if (typeof value[key] === 'undefined') {
+                    return path;
+                }
+                value = value[key];
+            }
+            
+            return value;
+        }
+        
+        return path;
     }
 }
