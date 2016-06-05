8607a8bde54476e37af0416780fdd42ecb6abdbf
Eugene Min <e.min@milax.com>
2016-06-05 12:34:43 +1100

    Make translations json inside javascript

--- src/js/jstrans.js
+++ src/js/jstrans.js
@@ -1,26 +1,20 @@
 var jstrans = function (path) {
-    // First try input
-    if (selector = document.querySelector('input[name="jstrans-' + path + '"]')) {
+    if (selector = document.querySelector('input[name=\"jstrans-value-for-' + path + '\"]')) {
         return selector.value;
     } else {
-        // Or textarea with huge languages array
-        if (selector = document.querySelector('textarea[name="jstrans"]')) {
-            var json = JSON.parse(selector.value);
-            var keys = path.split('.');
-            
-            var value = json;
-            
-            for (var i in keys) {
-                var key = keys[i];
-                if (typeof value[key] === 'undefined') {
-                    return path;
-                }
-                value = value[key];
+        var json = JSON.parse('%s');
+        var keys = path.split('.');
+        
+        var value = json;
+        
+        for (var i in keys) {
+            var key = keys[i];
+            if (typeof value[key] === 'undefined') {
+                return path;
             }
-            
-            return value;
+            value = value[key];
         }
         
-        return path;
+        return value;
     }
 }
--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,13 @@
 <?php
 
 Route::get('/js/jstrans.js', function () {
-    return response(file_get_contents(__DIR__ . '/js/jstrans.js'))->header('Content-Type', 'application/javascript');
+    foreach (\Config::get('jstrans') as $lang) {
+        $trans[$lang] = Lang::get($lang);
+    }
+    
+    $json = json_encode($trans);
+    
+    $js = sprintf(file_get_contents(__DIR__ . '/js/jstrans.js'), $json);
+    
+    return response($js)->header('Content-Type', 'application/javascript');
 });
